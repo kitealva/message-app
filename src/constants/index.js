118aeb94486b2b60c3519b65bd329ef493bf86f2
Brady <kite.alva@gmail.com>
2023-08-04 21:40:55 -0600

    nav-creation

---
+++ src/constants/index.js
@@ -0,0 +1,14 @@
+export const navLinks = [
+    {
+        id: "test",
+        title: "Test",
+    },
+    {
+        id: "test2",
+        title: "Test2",
+    },
+    {
+        id: "test3",
+        title: "Test3",
+    },
+];
